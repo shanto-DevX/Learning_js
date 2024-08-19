4b8cddb7818a1b4e38363f1d7da9b7102b2505d8
Saiful Islam Shanto <Helloshanto.DevX@gmail.com>
2024-08-19 16:36:46 +0600

    Learn js

--- Learn-Javascript/Problem Solving/04_multiplication tables.js
+++
@@ -1,7 +0,0 @@
-function multiplicationTables(num) {
-  for (let i = 1; i <= 10; i++) {
-    console.log(`${num} X ${i} = ${num * i}`);
-  }
-}
-
-multiplicationTables(5);
--- Learn-Javascript/Problem Solving/17_StringLengthHalf.js
+++
@@ -1,5 +0,0 @@
-const firstHalf = (str) => str.slice(0, str.length / 2);
-console.log(firstHalf("Hello"));
-console.log(firstHalf("HelloWorld"));
-console.log(firstHalf("HelloWorldjava"));
-console.log(firstHalf("HelloWorldjavaScript"));
